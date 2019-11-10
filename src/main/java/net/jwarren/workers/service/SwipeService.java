98f7927546a131071215868aa7378821d8c4617a
Josh <joshwarren@outlook.com.au>
2019-11-11 10:41:57 +1100

    Add SwipeService error handling

--- src/main/java/net/jwarren/workers/service/SwipeService.java
+++ src/main/java/net/jwarren/workers/service/SwipeService.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -26,25 +27,35 @@ public class SwipeService {
 
     public List<Job> findAllJobs() {
         LOGGER.info("GET {}", JOB_URL);
-        ResponseEntity<Job[]> jobs = restTemplate.getForEntity(JOB_URL, Job[].class);
-        Job[] body = jobs.getBody();
-        if (body != null) {
-            LOGGER.info("Got {} jobs", body.length);
-            return Arrays.asList(body);
+        try {
+            ResponseEntity<Job[]> jobs = restTemplate.getForEntity(JOB_URL, Job[].class);
+            Job[] body = jobs.getBody();
+            if (body != null) {
+                LOGGER.info("Got {} jobs", body.length);
+                return Arrays.asList(body);
+            }
+            LOGGER.warn("Got no jobs");
+        } catch (HttpClientErrorException exception) {
+            LOGGER.error("GET request failed, returning null:", exception);
         }
-        LOGGER.warn("Got no jobs");
+
         return null;
     }
 
     public List<Worker> findAllWorkers() {
         LOGGER.info("GET {}", WORKER_URL);
-        ResponseEntity<Worker[]> jobs = restTemplate.getForEntity(WORKER_URL, Worker[].class);
-        Worker[] body = jobs.getBody();
-        if (body != null) {
-            LOGGER.info("Got {} workers", body.length);
-            return Arrays.asList(body);
+        try {
+            ResponseEntity<Worker[]> jobs = restTemplate.getForEntity(WORKER_URL, Worker[].class);
+            Worker[] body = jobs.getBody();
+            if (body != null) {
+                LOGGER.info("Got {} workers", body.length);
+                return Arrays.asList(body);
+            }
+            LOGGER.warn("Got no workers");
+        } catch (HttpClientErrorException exception) {
+            LOGGER.error("GET request failed, returning null:", exception);
         }
-        LOGGER.warn("Got no workers");
+
         return null;
     }
 
